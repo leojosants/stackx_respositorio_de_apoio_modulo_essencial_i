ef401b2a654e964dcd8635fe5071e7fb27f93c34
Leonardo Santos <leonardojosantos@gmail.com>
2025-02-07 15:40:23 -0300

    update

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
 # [StackX](https://www.stackx.com.br/)
 
 ## Repositório de apoio - Modulo Essencial
-
-### Propostas de Tarefas
-
-- [01_tarefas_sem_data](https://github.com/leojosants/stackx_respositorio_de_apoio_modulo_essencial/tree/main/tarefas/01_tarefas_sem_data)
-- [02_tarefas_passadas](https://github.com/leojosants/stackx_respositorio_de_apoio_modulo_essencial/tree/main/tarefas/02_tarefas_passadas)
---
+++ tarefas/implementacoes_e_funcoes/objects.js
@@ -0,0 +1,63 @@
+// Calcula o perímetro e a área de um retângulo
+function calcularRetangulo(retangulo) {
+    // Sua implementação aqui
+
+    if (retangulo.altura === retangulo.largura) {
+        return "As medidas não são condizentes a um retângulo";
+    }
+
+    // P = 2(b+h)
+    const perimetro = 2 * (retangulo.largura + retangulo.altura);
+
+    // A = b*h
+    const area = retangulo.largura * retangulo.altura;
+
+    return {
+        perimetro, area,
+    };
+}
+const retangulo = {
+    largura: 2.3,
+    altura: 3.4,
+};
+calcularRetangulo(retangulo);
+
+
+// Verifica se uma pessoa é maior de idade
+function ehAdulto(pessoa) {
+    // Sua implementação aqui
+
+    let mensagem = '';
+    const idadeElegivel = 18;
+
+    if (pessoa.idade >= idadeElegivel) {
+        mensagem = 'É maior de idade.';
+    }
+    else {
+        mensagem = 'Não é maior de idade.';
+    }
+
+    return `Mensagem: ${mensagem}`;
+}
+const pessoa = {
+    nome: 'John',
+    idade: 25,
+    cidade: 'New York',
+};
+ehAdulto(pessoa);
+
+
+// Concatena os valores de um objeto em uma string
+function concatenaValores(obj) {
+    // Sua implementação aqui
+    const mensagem = `A linguagem ${obj.linguagem} é regulada pela ${obj.especificacao}.`;
+    return mensagem;
+}
+const obj = {
+    linguagem: 'JavaScript',
+    especificacao: 'ECMAScript',
+};
+concatenaValores(obj);
+
+
+module.exports = { calcularRetangulo, ehAdulto, concatenaValores };
